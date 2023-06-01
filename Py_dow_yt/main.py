ab94a8c906a271b65e7d8ecb510670b5e649c112
Зайцев Илья <ilyazaytsev2002@bk.ru>
2023-06-02 00:03:26 +0700

    Update main.py

--- Py_dow_yt/main.py
+++ Py_dow_yt/main.py
@@ -1,12 +1,15 @@
 from pytube import YouTube
 
+
 def download_video(url):
-    url_dow = YouTube(url).streams.first().download('C:/Users/Us1/Desktop/1')
+    url_dow = YouTube(url).streams.first().download('C:/Users/Us1/Desktop/video')
     return url_dow
+
 
 def main():
     url = input("Введите ссылку для скачаивания видео - ")
     download_video(url)
+
 
 if __name__ == "__main__":
     main()
