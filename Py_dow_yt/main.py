7f46b7e03a32b8e13dc101c2a7ca116ad6246cb0
Зайцев Илья <ilyazaytsev2002@bk.ru>
2023-06-27 16:59:39 +0700

    Update main.py

--- Py_dow_yt/main.py
+++ Py_dow_yt/main.py
@@ -1,19 +1,20 @@
 from pytube import YouTube
 from tkinter import *
 
-root = Tk()
 
-def download_video(url):
-    YouTube(url).streams.get_by_itag(22).download('C:/Users/Us1/Desktop/video')
-    url_dow = YouTube(url)
-    return url_dow
+# def download_video(url):
+#     YouTube(url).streams.get_by_itag(22).download('C:/Users/Us1/Desktop/video')
+#     url_dow = YouTube(url)
+#     return url_dow
 
 
 def main():
-    url = input("Введите ссылку для скачаивания видео - ")
-    download_video(url)
+    window = Tk()
+    #url = input("Введите ссылку для скачаивания видео - ")
+    #download_video(url)
+    window.geometry("800x460")
+    window.mainloop()
 
 
 if __name__ == "__main__":
     main()
-    root.mainloop()
