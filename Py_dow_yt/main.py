a12c9f46300afe45bab6a7f46452e2624b766a48
Зайцев Илья <ilyazaytsev2002@bk.ru>
2023-06-27 18:22:52 +0700

    Update main.py

--- Py_dow_yt/main.py
+++ Py_dow_yt/main.py
@@ -1,6 +1,26 @@
 from pytube import YouTube
 from tkinter import *
+from tkinter.ttk import *
 
+class Example(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.initUI()
+ 
+    def initUI(self):
+        self.parent.title("Скачивание видео из ютуб")
+        self.style = Style()
+        self.style.theme_use("default")
+ 
+        self.pack(fill=BOTH, expand=1)
+ 
+        quitButton = Button(self, text="Закрыть окно", command=self.quit)
+        
+        label = Label(text="Hello, Tkinter",foreground="white",background="black")
+        
+        label.place(x=300, y=50)
+        quitButton.place(x=100, y=50)
 
 # def download_video(url):
 #     YouTube(url).streams.get_by_itag(22).download('C:/Users/Us1/Desktop/video')
@@ -13,6 +33,7 @@ def main():
     #url = input("Введите ссылку для скачаивания видео - ")
     #download_video(url)
     window.geometry("800x460")
+    Example(window)
     window.mainloop()
 
 
